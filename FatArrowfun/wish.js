a3a4a9affab9bb9cf1aa698d26fd85e8768f90ac
praveentechorg17 <thathapudipraveen881@gmail.com>
2022-08-26 15:20:36 +0530

    ok

---
+++ FatArrowfun/wish.js
@@ -0,0 +1,40 @@
+/* function wish(){
+console.log("praveen,gm,gn")
+}
+wish()
+function wish(){
+    console.log("gopi,santhosh")
+}
+wish() */
+
+
+ /* var wish= ()=>{
+console.log("praveen,gopi,santhosh")
+ }
+ wish()
+ var wish=()=>{
+    console.log("GOOD MORNING,GOOD AFTERNOON,GOOD NIGHT")
+ }
+ wish(); */
+
+/*  function test (){
+console.log(this)
+ }
+ test() */
+
+/* let test=()=>{
+    console.log(this)
+}
+test() */
+
+let add = (a, b, c) => {
+    console.log(a + b + c)
+}
+add(10, 20, 30)
+/* 
+function add(a, b, c) {
+    console.log(a + b + c)
+}
+add(10, 20, 30) //60
+add(20, 30, 50) //100  */
+
