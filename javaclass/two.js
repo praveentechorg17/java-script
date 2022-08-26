d9546c5b4c08d234691268041842d194dc61a240
praveentechorg17 <thathapudipraveen881@gmail.com>
2022-08-26 17:09:26 +0530

    okk

---
+++ javaclass/two.js
@@ -0,0 +1,11 @@
+class Employee{
+    e_Id=101;
+    get_Emp_Details(){
+        return " displaying emp details"
+    }
+    
+}
+ let e1=new Employee();
+ let e2=new Employee(); 
+ console.log(e1.e_Id)
+ console.log(e1.get_Emp_Details())
