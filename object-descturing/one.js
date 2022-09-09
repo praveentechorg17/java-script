bbdd1ea099253aa620f9ef4c9625d556dca6773e
praveentechorg17 <thathapudipraveen881@gmail.com>
2022-09-09 11:36:24 +0530

    okk

---
+++ object-descturing/one.js
@@ -0,0 +1,11 @@
+let fruits={
+    
+            red:"apple",
+            yellow:"banana",
+            blue:"grapes"
+        
+         
+}
+// console.log(fruits.red)
+let {yellow,blue,red,orange}=fruits;
+console.log(yellow,blue,red,orange)
---
+++ object-descturing/two.js
@@ -0,0 +1,12 @@
+let fruits={
+    market:{
+           red:"apple",
+           blue:"grapees",
+           green:"guavava"  
+
+
+    }
+}
+// console.log(fruits.market.green)
+let {market}=fruits;
+console.log(market.red)
